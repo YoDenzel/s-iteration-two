fe11f4d51ba5026d48da5ddd7f786163f78cd954
Denis Dolbilin <kombogame6@gmail.com>
2022-04-27 17:39:15 +0200

    feat: ui for auth page

--- src/pages/auth-page/auth-page.tsx
+++ src/pages/auth-page/auth-page.tsx
@@ -1,3 +1,15 @@
+import { AuthWindow } from '../../components';
+import { Icons } from '../../shared/icons';
+import { Wrapper, TitleContainer, Title } from './emotion-components';
+
 export function AuthPage() {
-  return <div>Hello world</div>;
+  return (
+    <Wrapper component="main">
+      <TitleContainer>
+        <Icons.NeedForDriveLogo />
+        <Title variant="h3">Need for drive</Title>
+      </TitleContainer>
+      <AuthWindow />
+    </Wrapper>
+  );
 }
