07288ca3de509fedff72397d81cb95b649ac6017
Denis Dolbilin <kombogame6@gmail.com>
2022-04-28 13:59:11 +0200

    feat: redux provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,13 @@
 import { createTheme, ThemeProvider } from '@mui/material';
 import { ruRU } from '@mui/material/locale';
-import { Routes, Route } from 'react-router';
-import { AuthPage } from '../pages';
+import { Routes, Route, Navigate } from 'react-router';
+import { AuthPage, OrderListPage } from '../pages';
+import { useAppSelector } from '../shared/custom-hooks';
 
 export function App() {
+  const isAuthenticated = useAppSelector(
+    state => state.authSlice.isAuthenticated,
+  );
   const theme = createTheme(
     {
       palette: {
@@ -17,7 +21,22 @@ export function App() {
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        <Route path="*" element={<AuthPage />} />
+        {!isAuthenticated ? (
+          <Route path="/s-iteration-two/admin/*" element={<AuthPage />} />
+        ) : (
+          <>
+            <Route
+              path="/s-iteration-two/admin/order-list"
+              element={<OrderListPage />}
+            />
+            <Route
+              path="*"
+              element={
+                <Navigate to="/s-iteration-two/admin/order-list" replace />
+              }
+            />
+          </>
+        )}
       </Routes>
     </ThemeProvider>
   );
