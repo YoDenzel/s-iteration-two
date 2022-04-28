d93e32d99f75269de07c89ced29aeaa39a0ed592
Denis Dolbilin <kombogame6@gmail.com>
2022-04-28 14:00:01 +0200

    feat: redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 import './index.scss';
 import { App } from './app/App';
+import { store } from './redux/store';
 import './fonts/Roboto/Roboto-Black.ttf';
 import './fonts/Roboto/Roboto-Bold.ttf';
 import './fonts/Roboto/Roboto-Light.ttf';
 import './fonts/Roboto/Roboto-Medium.ttf';
 import './fonts/Roboto/Roboto-Regular.ttf';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
@@ -18,9 +20,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
