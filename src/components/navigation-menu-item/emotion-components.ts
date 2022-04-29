679175daaecca7c629640e585ed69ffcb487a02e
Denis Dolbilin <kombogame6@gmail.com>
2022-04-29 18:35:40 +0200

    refactor: files for styling and for types

---
+++ src/components/navigation-menu-item/emotion-components.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import { Button, Typography } from '@mui/material';
+
+export const Container = styled(Button)`
+  display: flex;
+  gap: 14px;
+  box-shadow: inset 0px -1px 0px #e1e5eb;
+  justify-content: flex-start;
+  padding-left: 28px;
+`;
+
+export const Title = styled(Typography)`
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 17px;
+  padding: 20px 0;
+`;
