de3f4cd5f58d6bffdac34de3067ff30ee4b6b2b6
Denis Dolbilin <kombogame6@gmail.com>
2022-04-29 18:51:19 +0200

    feat: exports

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -1,5 +1,11 @@
 import { NeedForDriveLogo } from './need-for-drive-logo';
+import { CarCardPen } from './car-card-pen';
+import { CarList } from './car-list';
+import { Orders } from './orders';
 
 export const Icons = {
   NeedForDriveLogo,
+  CarCardPen,
+  CarList,
+  Orders,
 };
