30b67d2f40838b44b5fa5b0b495bdc22f100557a
Denis Dolbilin <kombogame6@gmail.com>
2022-04-29 22:26:33 +0200

    feat: exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { AuthWindow } from './auth-window';
 export { TextInputComponent } from './text-input-component';
 export { NavigationMenu } from './navigation-menu';
 export { NavigationMenuItem } from './navigation-menu-item';
+export { AdminPanelHeader } from './admin-panel-header';
