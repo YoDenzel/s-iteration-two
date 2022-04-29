dbc510e3f9aedeef17eb853c16bd98d4b7cd32fc
Denis Dolbilin <kombogame6@gmail.com>
2022-04-29 19:33:45 +0200

    feat: added simple logout button

--- src/pages/order-list-page/order-list-page.tsx
+++ src/pages/order-list-page/order-list-page.tsx
@@ -1,5 +1,19 @@
+import { useCookies } from 'react-cookie';
+
 export function OrderListPage() {
-    return (<div>
-        Hello world
-    </div>)
+  const [_, __, removeCookie] = useCookies(['access']);
+  return (
+    <div>
+      Hello world
+      <button
+        onClick={() =>
+          removeCookie('access', {
+            path: '/',
+          })
+        }
+      >
+        Клик
+      </button>
+    </div>
+  );
 }
