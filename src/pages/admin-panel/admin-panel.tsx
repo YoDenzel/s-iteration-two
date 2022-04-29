ddfc96a905c28f2be065748779a166e7c6e13b7d
Denis Dolbilin <kombogame6@gmail.com>
2022-04-29 22:27:19 +0200

    feat: added header component

--- src/pages/admin-panel/admin-panel.tsx
+++ src/pages/admin-panel/admin-panel.tsx
@@ -1,26 +1,30 @@
 import styled from '@emotion/styled';
 import { Box } from '@mui/system';
-import { addDays } from 'date-fns';
-import { useEffect, useState } from 'react';
-import { useCookies } from 'react-cookie';
-import { NavigationMenu } from '../../components';
+import { useState } from 'react';
+import { AdminPanelHeader, NavigationMenu } from '../../components';
 import { useAppSelector } from '../../shared/custom-hooks';
+
+const Wrapper = styled(Box)`
+  flex: 1;
+  display: flex;
+`;
 
 const Container = styled(Box)`
   flex: 1;
-  display: flex;
 `;
 
 export function AdminPanel() {
   const [activeIndex, setActiveIndex] = useState(0);
   const authObj = useAppSelector(state => state.authSlice.authObj);
   return (
-    <Container>
+    <Wrapper>
       <NavigationMenu
         activeIndex={activeIndex}
         setActiveIndex={setActiveIndex}
       />
-      Hello world
-    </Container>
+      <Container>
+        <AdminPanelHeader />
+      </Container>
+    </Wrapper>
   );
 }
