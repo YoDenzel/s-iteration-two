cb0819b238e41d60f94cd9a3fc5b391975cc6441
Denis Dolbilin <kombogame6@gmail.com>
2022-05-01 15:52:39 +0200

    refactor: moved emotion components to separate file

--- src/components/admin-panel-footer/admin-panel-footer.tsx
+++ src/components/admin-panel-footer/admin-panel-footer.tsx
@@ -1,39 +1,4 @@
-import styled from '@emotion/styled';
-import { Box, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
-
-const Container = styled(Box)`
-  background: #ffffff;
-  box-shadow: 0px -0.5px 0px #d7dae3;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const NavBlock = styled(Box)`
-  padding: 27px 0 25.5px 27.5px;
-  display: flex;
-  gap: 16px;
-`;
-
-const NavLink = styled(Link)`
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 16px;
-  text-decoration: none;
-  color: #007bff;
-`;
-
-const Copyright = styled(Typography)`
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 16px;
-  text-align: right;
-  color: #818ea3;
-  padding-right: 27.5px;
-`;
+import { Container, Copyright, NavBlock, NavLink } from './emotion-components';
 
 export function AdminPanelFooter() {
   return (
---
+++ src/components/admin-panel-footer/emotion-components.ts
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+import { Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+export const Container = styled(Box)`
+  background: #ffffff;
+  box-shadow: 0px -0.5px 0px #d7dae3;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const NavBlock = styled(Box)`
+  padding: 27px 0 25.5px 27.5px;
+  display: flex;
+  gap: 16px;
+`;
+
+export const NavLink = styled(Link)`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  text-decoration: none;
+  color: #007bff;
+`;
+
+export const Copyright = styled(Typography)`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: right;
+  color: #818ea3;
+  padding-right: 27.5px;
+`;
