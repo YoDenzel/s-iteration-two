ff7ddba53bc4c148c744eea8a52940a2a6129d7a
Denis Dolbilin <kombogame6@gmail.com>
2022-05-01 15:45:39 +0200

    feat: added component for all the data on car orders

---
+++ src/components/orders-list-component/orders-list-component.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
+
+const Container = styled(Box)`
+  display: flex;
+  flex: 1;
+`;
+
+export function OrdersListComponent() {
+  return <Container component="main">Hello world</Container>;
+}
