0e826060e6bce73b3828e3280e7906180df6d872
Denis Dolbilin <kombogame6@gmail.com>
2022-05-01 15:48:03 +0200

    feat: exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { TextInputComponent } from './text-input-component';
 export { NavigationMenu } from './navigation-menu';
 export { NavigationMenuItem } from './navigation-menu-item';
 export { AdminPanelHeader } from './admin-panel-header';
+export { OrdersListComponent } from './orders-list-component';
+export { AdminPanelFooter } from './admin-panel-footer';
--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,2 +1,3 @@
 export { fetchPostAuth } from './fetch-post-auth';
 export { fetchPostLogout } from './fetch-post-logout';
+export { fetchData } from './fetch-get-data';
