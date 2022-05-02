26b29b8d5246e10e8fc4d83803ef3d4c47f2f82c
Denis Dolbilin <kombogame6@gmail.com>
2022-05-02 11:54:39 +0200

    feat: types for rate request

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -16,3 +16,25 @@ export type TPostAuthResponse = {
   refresh_token: string;
   user_id: string;
 };
+
+export type TCarRate = {
+  count: number;
+  data: TCarRateData[];
+  fields: {};
+};
+
+export type TCarRateData = {
+  createdAt: number;
+  id: string;
+  price: number;
+  rateTypeId: TCarRateTypeId;
+  updatedAt: number;
+};
+
+type TCarRateTypeId = {
+  id: string;
+  name: string;
+  unit: string;
+};
+
+export type TOptionsArr = { title: string; optionId: string };
