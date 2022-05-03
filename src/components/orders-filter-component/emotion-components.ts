beb741c44e138730cfe20139e1d9ba53b72f5681
Denis Dolbilin <kombogame6@gmail.com>
2022-05-03 16:32:22 +0200

    feat: deleted setState prop, moved all the emotion components to separate styling file

---
+++ src/components/orders-filter-component/emotion-components.ts
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import { Box, Button } from '@mui/material';
+
+export const Wrapper = styled(Box)`
+  width: 100%;
+  background-color: #ffffff;
+  display: flex;
+  padding: 16px 0px 14px 21px;
+  box-shadow: 0px -0.5px 0px #e0e2e8, 0px 0.5px 0px #e0e2e8;
+  justify-content: space-between;
+  border-radius: inherit;
+`;
+
+export const FilterButtonContainer = styled(Box)`
+  display: flex;
+  gap: 15px;
+`;
+
+export const ApplyFilterButton = styled(Button)`
+  font-family: 'Helvetica';
+  font-style: normal;
+  background: #007bff;
+  font-weight: 400;
+  font-size: 11px;
+  line-height: 13px;
+  color: #ffffff;
+  text-transform: none;
+  &:hover {
+    background: #007bff;
+  }
+  margin-right: 21px;
+  padding: 8px 15px 8.5px 15px;
+  border-radius: 4px;
+`;
--- src/components/orders-filter-component/orders-filter-component.tsx
+++ src/components/orders-filter-component/orders-filter-component.tsx
@@ -1,39 +1,10 @@
-import styled from '@emotion/styled';
-import { Box, Button } from '@mui/material';
 import { FilterButtonComponent } from '../filter-button-component';
+import {
+  Wrapper,
+  FilterButtonContainer,
+  ApplyFilterButton,
+} from './emotion-components';
 import { TOrdersFilterComponent } from './types';
-
-const Wrapper = styled(Box)`
-  width: 100%;
-  background-color: #ffffff;
-  display: flex;
-  padding: 16px 0px 14px 21px;
-  box-shadow: 0px -0.5px 0px #e0e2e8, 0px 0.5px 0px #e0e2e8;
-  justify-content: space-between;
-  border-radius: inherit;
-`;
-
-const FilterButtonContainer = styled(Box)`
-  display: flex;
-  gap: 15px;
-`;
-
-const ApplyFilterButton = styled(Button)`
-  font-family: 'Helvetica';
-  font-style: normal;
-  background: #007bff;
-  font-weight: 400;
-  font-size: 11px;
-  line-height: 13px;
-  color: #ffffff;
-  text-transform: none;
-  &:hover {
-    background: #007bff;
-  }
-  margin-right: 21px;
-  padding: 8px 15px 8.5px 15px;
-  border-radius: 4px;
-`;
 
 export function OrderFilterComponent({
   filterDataArr,
@@ -48,7 +19,7 @@ export function OrderFilterComponent({
             optionsArr={item.data}
             buttonTitle={item.title}
             selectedOption={item.activeTitle?.name}
-            setObj={item.setActiveObj}
+            activeIndex={index}
           />
         ))}
       </FilterButtonContainer>
