090116fb4f91fc39ea3283ebb1578e8b05c1edf1
Denis Dolbilin <kombogame6@gmail.com>
2022-05-03 16:31:38 +0200

    feat: not using setState as a member of an object anymore

--- src/shared/functions/filter-data-arr/filter-data-arr.ts
+++ src/shared/functions/filter-data-arr/filter-data-arr.ts
@@ -1,37 +1,26 @@
 import { TFilterDataArr } from './types';
 
-export const filterDataArr = ({
-  data,
-  activeObjArr,
-  setActiveCitiesObj,
-  setActiveRateObj,
-  setActiveStatusObj,
-  setActiveCarObj,
-}: TFilterDataArr) => {
+export const filterDataArr = ({ data, activeObjArr }: TFilterDataArr) => {
   return [
     {
       title: 'Тариф',
       data: data[0],
       activeTitle: activeObjArr[0],
-      setActiveObj: setActiveRateObj,
     },
     {
       title: 'Города',
       data: data[1],
       activeTitle: activeObjArr[1],
-      setActiveObj: setActiveCitiesObj,
     },
     {
       title: 'Статус',
       data: data[2],
       activeTitle: activeObjArr[2],
-      setActiveObj: setActiveStatusObj,
     },
     {
       title: 'Машина',
       data: data[3],
       activeTitle: activeObjArr[3],
-      setActiveObj: setActiveCarObj,
     },
   ];
 };
--- src/shared/functions/filter-data-arr/types.ts
+++ src/shared/functions/filter-data-arr/types.ts
@@ -3,8 +3,4 @@ import { TOptionsArr } from '../../types';
 export type TFilterDataArr = {
   data: Array<TOptionsArr[] | undefined>;
   activeObjArr: Array<TOptionsArr | null>;
-  setActiveRateObj: (v: TOptionsArr | null) => void;
-  setActiveCitiesObj: (v: TOptionsArr | null) => void;
-  setActiveStatusObj: (v: TOptionsArr | null) => void;
-  setActiveCarObj: (v: TOptionsArr | null) => void;
 };
