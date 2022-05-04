4a60c488952e3c276e3e12aaf4ea59848be4f688
Denis Dolbilin <kombogame6@gmail.com>
2022-05-04 11:02:14 +0200

    fix: now after logout changing route

--- src/pages/order-list-page/order-list-page.tsx
+++ src/pages/order-list-page/order-list-page.tsx
@@ -1,16 +1,22 @@
+import { addDays } from 'date-fns';
 import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 export function OrderListPage() {
-  const [_, __, removeCookie] = useCookies(['access']);
+  const [cookie, __, removeCookie] = useCookies(['access']);
+  const navigate = useNavigate();
+
   return (
     <div>
       Hello world
       <button
-        onClick={() =>
+        onClick={() => {
           removeCookie('access', {
             path: '/',
-          })
-        }
+            expires: addDays(new Date(), 1),
+          });
+          navigate('/s-iteration-two/admin');
+        }}
       >
         Клик
       </button>
