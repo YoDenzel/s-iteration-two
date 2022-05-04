f97ed90f1f678970e1cb71f4785a10beea412fb9
Denis Dolbilin <kombogame6@gmail.com>
2022-05-04 23:31:26 +0200

    refactor: moved all the emotion components to separate file

--- src/components/navigation-menu/navigation-menu.tsx
+++ src/components/navigation-menu/navigation-menu.tsx
@@ -1,36 +1,10 @@
-import styled from '@emotion/styled';
-import { Box, Typography } from '@mui/material';
 import { Icons } from '../../shared/icons';
-
-const Container = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  width: 255px;
-  background-color: #ffffff;
-  height: 100vh;
-`;
-
-const ApplicationTitleWrapper = styled(Box)`
-  width: 100%;
-  box-shadow: 0px 1px 0px #e1e5eb;
-`;
-
-const ApplicationTitleContainer = styled(Box)`
-  display: flex;
-  align-items: center;
-  gap: 7px;
-  padding: 23px 0;
-  padding-left: 49px;
-`;
-
-const Title = styled(Typography)`
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 18px;
-  display: flex;
-  align-items: center;
-  color: #3d5170;
-`;
+import {
+  Container,
+  ApplicationTitleWrapper,
+  ApplicationTitleContainer,
+  Title,
+} from './emotion-components';
 
 export function NavigationMenu() {
   return (
