841b8563f4422f84149db4ee7e4a80bd6bedfbf8
Denis Dolbilin <kombogame6@gmail.com>
2022-05-04 10:30:37 +0200

    feat: checkmark for checkboxes

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -6,6 +6,7 @@ import { NotificationBell } from './notifications-bell';
 import { DropdownArrow } from './dropdown-arrow';
 import { SearchLoop } from './search-loop';
 import { DropdownBothWaysArrow } from './dropdown-both-ways-arrow';
+import { CheckMark } from './check-mark';
 
 export const Icons = {
   NeedForDriveLogo,
@@ -16,4 +17,5 @@ export const Icons = {
   DropdownArrow,
   SearchLoop,
   DropdownBothWaysArrow,
+  CheckMark,
 };
