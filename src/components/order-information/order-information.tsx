ab30320d46e22efa79b320cfed324fd56ce7ccde
Denis Dolbilin <kombogame6@gmail.com>
2022-05-06 17:45:23 +0200

    feat: added change data button elements

--- src/components/order-information/order-information.tsx
+++ src/components/order-information/order-information.tsx
@@ -1,13 +1,20 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import styled from '@emotion/styled';
 import { TTitleStyle, TOrderInformation } from './types';
 import { CheckboxItem } from '../checkbox-item';
 import { useAppSelector } from '../../shared/custom-hooks';
+import { OrderChangeStatusData } from './constants';
 
 const Container = styled(Box)`
   display: flex;
-  padding-left: 21px;
-  padding-right: 21px;
+  padding-left: ${({ theme }) => theme.spacing(2.625)}px;
+  padding-right: ${({ theme }) => theme.spacing(2.625)}px;
+  padding-top: ${({ theme }) => theme.spacing(2)}px;
+`;
+
+const CarInfoWrapper = styled(Box)`
+  display: flex;
+  flex: 40%;
 `;
 
 const CarInfoBlock = styled(Box)`
@@ -15,21 +22,19 @@ const CarInfoBlock = styled(Box)`
   width: 230px;
   flex-wrap: wrap;
   text-align: left;
-  margin-top: 15.5px;
 `;
 
 const CarImg = styled.img`
   width: 138px;
   height: auto;
-  margin-top: 13.5px;
-  margin-right: 16px;
+  margin-right: ${({ theme }) => theme.spacing(2)}px;
 `;
 
 const CheckboxBlock = styled(Box)`
   display: flex;
   flex-direction: column;
-  gap: 7px;
-  margin-top: 13.5px;
+  gap: ${({ theme }) => theme.spacing(1)}px;
+  flex: 20%;
 `;
 
 const Title = styled(({ isBlack, children, ...props }: TTitleStyle) => (
@@ -44,6 +49,57 @@ const Title = styled(({ isBlack, children, ...props }: TTitleStyle) => (
   padding-right: 5px;
 `;
 
+const Price = styled(Typography)`
+  font-family: 'Helvetica';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+  letter-spacing: -0.754286px;
+  color: ${({ theme }) => theme.colors.black};
+  padding-top: ${({ theme }) => theme.spacing(1.5)}px;
+  flex: 20%;
+`;
+
+const ChangeStatusBlock = styled(Box)`
+  display: flex;
+  margin-top: ${({ theme }) => theme.spacing(1.5)}px;
+  align-items: flex-start;
+  border: 0.5px solid ${({ theme }) => theme.colors.inputBorder};
+  border-radius: ${({ theme }) => theme.spacing(0.5)}px;
+  height: fit-content;
+  @media (max-width: 1200px) {
+    margin-top: 0;
+    flex-direction: column;
+  }
+`;
+
+const ChangeStatusButton = styled(Button)<{ activeindex: number }>`
+  border: none;
+  font-family: 'Helvetica';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 11px;
+  line-height: 13px;
+  text-align: left;
+  letter-spacing: -0.345714px;
+  color: ${({ theme }) => theme.colors.secondaryFont};
+  text-transform: none;
+  border-left: ${({ activeindex }) =>
+    activeindex === 1 ? '0.5px solid #BECAD6' : 'none'};
+  border-right: ${({ activeindex }) =>
+    activeindex === 1 ? '0.5px solid #BECAD6' : 'none'};
+  border-radius: 0;
+  margin: 0;
+  padding: 6px 15px;
+  &:hover {
+    background: none;
+  }
+  @media (max-width: ${({ theme }) => theme.breakPoints.desktopMin}) {
+    border: none;
+  }
+`;
+
 export function OrderInformation({
   activeOrderObj,
   dateFrom,
@@ -54,23 +110,25 @@ export function OrderInformation({
   );
   return (
     <Container component="section">
-      <CarImg src={activeOrderObj?.carId.thumbnail.path} />
-      <CarInfoBlock>
-        <Title isBlack={true}>{activeOrderObj?.carId.name}</Title>
-        <Title isBlack={false}>в</Title>
-        <Title isBlack={true}>
-          {(activeOrderObj?.cityId?.name ?? 'Нет информации') + ','}
-        </Title>
-        <Title isBlack={false}>
-          {(activeOrderObj?.pointId !== null &&
-            activeOrderObj?.pointId.address) ||
-            ''}
-        </Title>
-        <Title isBlack={false}>{dateFrom + ' - ' + dateTo}</Title>
-        <Title isBlack={false}>Цвет:</Title>
-        <Title isBlack>{activeOrderObj?.color}</Title>
-      </CarInfoBlock>
-      <CheckboxBlock>
+      <CarInfoWrapper>
+        <CarImg src={activeOrderObj?.carId.thumbnail.path} />
+        <CarInfoBlock component="article">
+          <Title isBlack={true}>{activeOrderObj?.carId.name}</Title>
+          <Title isBlack={false}>в</Title>
+          <Title isBlack={true}>
+            {(activeOrderObj?.cityId?.name ?? 'Нет информации') + ','}
+          </Title>
+          <Title isBlack={false}>
+            {(activeOrderObj?.pointId !== null &&
+              activeOrderObj?.pointId.address) ||
+              ''}
+          </Title>
+          <Title isBlack={false}>{dateFrom + ' - ' + dateTo}</Title>
+          <Title isBlack={false}>Цвет:</Title>
+          <Title isBlack>{activeOrderObj?.color}</Title>
+        </CarInfoBlock>
+      </CarInfoWrapper>
+      <CheckboxBlock component="article">
         {checkboxArr.map((item, index) => (
           <CheckboxItem
             key={item.title + index}
@@ -79,6 +137,18 @@ export function OrderInformation({
           />
         ))}
       </CheckboxBlock>
+      <Price>${activeOrderObj?.price}</Price>
+      <ChangeStatusBlock component="article">
+        {OrderChangeStatusData.map((item, index) => (
+          <ChangeStatusButton
+            activeindex={index}
+            key={item.title + index}
+            startIcon={<item.icon color={index === 0 ? '#17C671 ' : ''} />}
+          >
+            {item.title}
+          </ChangeStatusButton>
+        ))}
+      </ChangeStatusBlock>
     </Container>
   );
 }
