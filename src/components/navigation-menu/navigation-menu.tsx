5975f81d0157b286de360d69c72b77a979705451
Denis Dolbilin <kombogame6@gmail.com>
2022-05-11 20:53:18 +0300

    feat: added props for adaptivity of the menu

--- src/components/navigation-menu/navigation-menu.tsx
+++ src/components/navigation-menu/navigation-menu.tsx
@@ -1,4 +1,5 @@
 import { Icons } from '../../shared/icons';
+import { MobileOpenMenuButton } from '../mobile-open-menu-button';
 import { NavigationMenuItem } from '../navigation-menu-item';
 import { menuTabsDataArr } from './constants';
 import {
@@ -6,33 +7,49 @@ import {
   ApplicationTitleContainer,
   ApplicationTitleWrapper,
   Title,
+  TabsContainer,
 } from './emotion-components';
 import { TNavigationMen } from './types';
 
 export function NavigationMenu({
   activeIndex,
   setActiveIndex,
+  isMenuActive,
+  setIsMenuActive,
+  windowWidth,
+  mobileMenuRef,
 }: TNavigationMen) {
   return (
-    <Container component="nav">
+    <Container component="nav" ref={mobileMenuRef}>
       <ApplicationTitleWrapper>
         <ApplicationTitleContainer>
           <Icons.NeedForDriveLogo />
           <Title>Need for car</Title>
         </ApplicationTitleContainer>
+        {windowWidth < 1024 && (
+          <MobileOpenMenuButton
+            clickHandler={() => setIsMenuActive(!isMenuActive)}
+          />
+        )}
       </ApplicationTitleWrapper>
-      {menuTabsDataArr.map((item, index) => (
-        <NavigationMenuItem
-          key={item.title + index}
-          clickHandler={setActiveIndex}
-          Icon={
-            <item.icon color={activeIndex === index ? '#007BFF' : '#CACEDB'} />
-          }
-          title={item.title}
-          activeIndex={activeIndex}
-          itemIndex={index}
-        />
-      ))}
+      {isMenuActive && (
+        <TabsContainer>
+          {menuTabsDataArr.map((item, index) => (
+            <NavigationMenuItem
+              key={item.title + index}
+              clickHandler={setActiveIndex}
+              Icon={
+                <item.icon
+                  color={activeIndex === index ? '#007BFF' : '#CACEDB'}
+                />
+              }
+              title={item.title}
+              activeIndex={activeIndex}
+              itemIndex={index}
+            />
+          ))}
+        </TabsContainer>
+      )}
     </Container>
   );
 }
--- src/components/navigation-menu/types.ts
+++ src/components/navigation-menu/types.ts
@@ -1,4 +1,8 @@
 export type TNavigationMen = {
   activeIndex: number;
   setActiveIndex: (v: number) => void;
+  windowWidth: number;
+  isMenuActive: boolean;
+  setIsMenuActive: (v: boolean) => void;
+  mobileMenuRef?: React.RefObject<HTMLDivElement>;
 };
