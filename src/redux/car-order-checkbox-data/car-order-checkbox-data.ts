a6e2b10d41dfac3cf78cb66620cdd3aad54d0513
Denis Dolbilin <kombogame6@gmail.com>
2022-05-11 20:51:01 +0300

    feat: added action for setting checkboxes status (true/false)

--- src/redux/car-order-checkbox-data/car-order-checkbox-data.ts
+++ src/redux/car-order-checkbox-data/car-order-checkbox-data.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { TSetCheckboxesStatus } from './types';
 
 export const carOrderCheckboxData = createSlice({
   name: 'carOrderCheckboxData',
@@ -6,22 +7,30 @@ export const carOrderCheckboxData = createSlice({
     checkboxArr: [
       {
         title: 'Полный бак',
-        isActive: false,
+        isActive: null as boolean | null,
         price: 500,
       },
       {
         title: 'Детское кресло',
-        isActive: false,
+        isActive: null as boolean | null,
         price: 200,
       },
       {
         title: 'Правый руль',
-        isActive: false,
+        isActive: null as boolean | null,
         price: 1600,
       },
     ],
   },
   reducers: {
+    setCheckboxesStatus: (
+      state,
+      action: PayloadAction<TSetCheckboxesStatus>,
+    ) => {
+      (state.checkboxArr[0].isActive = action.payload.isTankFull),
+        (state.checkboxArr[1].isActive = action.payload.isChildChair);
+      state.checkboxArr[2].isActive = action.payload.isRightWheel;
+    },
     setCheckboxArr: (state, action: PayloadAction<{ activeTitle: string }>) => {
       state.checkboxArr = state.checkboxArr.map(item => {
         if (item.title === action.payload.activeTitle) {
@@ -32,6 +41,7 @@ export const carOrderCheckboxData = createSlice({
   },
 });
 
-export const { setCheckboxArr } = carOrderCheckboxData.actions;
+export const { setCheckboxArr, setCheckboxesStatus } =
+  carOrderCheckboxData.actions;
 
 export default carOrderCheckboxData.reducer;
