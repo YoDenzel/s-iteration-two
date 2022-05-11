811532071766bcb5ab62602b38ed8b7cb824f4a8
Denis Dolbilin <kombogame6@gmail.com>
2022-05-11 20:55:32 +0300

    feat: export for useClickOutside hook

--- src/shared/custom-hooks/index.ts
+++ src/shared/custom-hooks/index.ts
@@ -2,3 +2,4 @@ export { usePostAuth } from './use-post-auth';
 export { useAppDispatch, useAppSelector } from './custom-redux-hooks';
 export { useClickOutside } from './use-click-outside';
 export { usePostLogout } from './use-post-logout';
+export { useGetWindowWidth } from './use-get-window-width';
