b3a9a4002778118d744baf29a855c16e945cbacc
Denis Dolbilin <kombogame6@gmail.com>
2022-05-13 18:58:05 +0300

    feat: some styling

--- src/components/navigation-menu/emotion-components.ts
+++ src/components/navigation-menu/emotion-components.ts
@@ -14,7 +14,7 @@ export const Container = styled(Box)`
   @media (max-width: ${({ theme }) => theme.breakPoints.tablet}) {
     height: fit-content;
     position: absolute;
-    width: 240px;
+    width: 100%;
   }
 `;
 
@@ -22,6 +22,9 @@ export const ApplicationTitleWrapper = styled(Box)`
   width: 100%;
   box-shadow: 0px 1px 0px #e1e5eb;
   display: flex;
+  @media (max-width: ${({ theme }) => theme.breakPoints.tablet}) {
+    justify-content: space-between;
+  }
 `;
 
 export const ApplicationTitleContainer = styled(Box)`
@@ -30,12 +33,13 @@ export const ApplicationTitleContainer = styled(Box)`
   padding: 15px 0;
   gap: ${({ theme }) => theme.spacing(1)}px;
   padding-left: 49px;
+  @media (max-width: ${({ theme }) => theme.breakPoints.mobile}) {
+    padding-left: 15px;
+  }
 `;
 
 export const TabsContainer = styled(Box)`
   width: 100%;
-  @media (max-width: ${({ theme }) => theme.breakPoints.tablet}) {
-  }
 `;
 
 export const Title = styled(Typography)`
