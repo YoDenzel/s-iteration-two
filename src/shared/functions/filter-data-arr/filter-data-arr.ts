c614d0c34bc032effd973285bbfd7b126a8c7ca9
Denis Dolbilin <kombogame6@gmail.com>
2022-05-16 18:05:34 +0300

    fix: filtering undefined

--- src/shared/functions/filter-data-arr/filter-data-arr.ts
+++ src/shared/functions/filter-data-arr/filter-data-arr.ts
@@ -4,22 +4,22 @@ export const filterDataArr = ({ data, activeObjArr }: TFilterDataArr) => {
   return [
     {
       title: 'Тариф',
-      data: data[0],
+      data: data[0]?.filter(val => val.name !== undefined),
       activeTitle: activeObjArr[0],
     },
     {
       title: 'Города',
-      data: data[1],
+      data: data[1]?.filter(val => val.name !== undefined),
       activeTitle: activeObjArr[1],
     },
     {
       title: 'Статус',
-      data: data[2],
+      data: data[2]?.filter(val => val.name !== undefined),
       activeTitle: activeObjArr[2],
     },
     {
       title: 'Машина',
-      data: data[3],
+      data: data[3]?.filter(val => val.name !== undefined),
       activeTitle: activeObjArr[3],
     },
   ];
