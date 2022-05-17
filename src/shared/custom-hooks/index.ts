69d97c25fb0e7be24ab96e3cb1b4840d3aa7e14a
Denis Dolbilin <kombogame6@gmail.com>
2022-05-17 14:56:59 +0300

    feat: exports for pagination logic

--- src/shared/custom-hooks/index.ts
+++ src/shared/custom-hooks/index.ts
@@ -3,3 +3,4 @@ export { useAppDispatch, useAppSelector } from './custom-redux-hooks';
 export { useClickOutside } from './use-click-outside';
 export { usePostLogout } from './use-post-logout';
 export { useGetWindowWidth } from './use-get-window-width';
+export { usePagination } from './use-pagination';
