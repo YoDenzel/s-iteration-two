1dbb5dbefcb64dbc2aefb197274fe1389a84db7f
Denis Dolbilin <kombogame6@gmail.com>
2022-05-17 23:55:12 +0300

    feat: moved emotion components, types to separate files

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,88 +1,12 @@
-import styled from '@emotion/styled';
-import { Box, Button } from '@mui/material';
 import { Icons } from '../../shared/icons';
-
-type TPagination = {
-  currentPage: number;
-  setCurrentPage: (v: number) => void;
-  paginationRange: (string | number)[] | undefined;
-  nextPageClickhandler: () => void;
-  prevPageClickhandler: () => void;
-  isPrevPage: () => boolean;
-  isNextPage: () => boolean;
-};
-
-export const Container = styled(Box)`
-  margin-bottom: 15px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const PrevPage = styled(Button)<{ isactive: number }>`
-  font-family: 'Helvetica';
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  width: 40px;
-  height: 40px;
-  margin: 0 3px;
-  color: var(--black);
-  text-align: center;
-  display: inline-flex;
-  border: 1px solid transparent;
-  padding: 0px 6px;
-  cursor: ${({ isactive }) => (isactive ? 'pointer' : 'default')};
-  min-width: unset;
-  &:hover {
-    border-radius: 16px;
-    border: ${({ isactive }) => (isactive ? '1px solid var(--gray)' : '0px')};
-  }
-`;
-
-export const Dots = styled(Box)``;
-
-export const PaginationItem = styled(Button)<{ isactive: number }>`
-  min-width: unset;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  margin: 0 3px;
-  color: ${({ isactive }) => (isactive ? '#ffffff' : ' #007BFF')};
-  text-align: center;
-  display: inline-flex;
-  width: 40px;
-  height: 40px;
-  padding: 0px 6px;
-  background-color: ${({ isactive }) => (isactive ? ' #007BFF' : 'none')};
-  border-radius: 50%;
-  &:hover {
-    background-color: ${({ isactive }) => (isactive ? '#007BFF' : 'none')};
-  }
-`;
-
-export const NextPage = styled(Button)<{ isactive: number }>`
-  font-family: 'Helvetica';
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  margin: 0 3px;
-  color: var(--black);
-  text-align: center;
-  display: inline-flex;
-  width: 40px;
-  height: 40px;
-  border: 1px solid transparent;
-  padding: 0px 10px;
-  cursor: ${({ isactive }) => (isactive ? 'pointer' : 'default')};
-  &:hover {
-    border-radius: 16px;
-    border: ${({ isactive }) => (isactive ? '1px solid var(--gray)' : '0px')};
-  }
-  transform: rotate(180deg);
-  min-width: unset;
-`;
+import {
+  PrevPage,
+  Dots,
+  NextPage,
+  Container,
+  PaginationItem,
+} from './emotion-components';
+import { TPagination } from './types';
 
 export function Pagination({
   currentPage,
@@ -99,6 +23,7 @@ export function Pagination({
         disabled={isPrevPage()}
         onClick={() => prevPageClickhandler()}
         isactive={!isPrevPage() ? 1 : 0}
+        disableRipple
       >
         <Icons.ChangePage />
       </PrevPage>
@@ -110,7 +35,6 @@ export function Pagination({
             </Dots>
           );
         }
-
         return (
           <PaginationItem
             key={Number(item) + index}
@@ -125,7 +49,8 @@ export function Pagination({
       <NextPage
         onClick={() => nextPageClickhandler()}
         disabled={isNextPage()}
-        isactive={isNextPage() ? 1 : 0}
+        isactive={!isNextPage() ? 1 : 0}
+        disableRipple
       >
         <Icons.ChangePage />
       </NextPage>
