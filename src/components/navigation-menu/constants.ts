1ff235e341f632ee47580431eaab90200363f0bf
Denis Dolbilin <kombogame6@gmail.com>
2022-05-18 16:30:26 +0300

    feat: added all entities name and icons

--- src/components/navigation-menu/constants.ts
+++ src/components/navigation-menu/constants.ts
@@ -1,13 +1,20 @@
-import { Icons } from '../../shared/icons';
+import HouseIcon from '@mui/icons-material/House';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import AddCardIcon from '@mui/icons-material/AddCard';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
+import CategoryIcon from '@mui/icons-material/Category';
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
 
 export const menuTabsDataArr = [
   {
-    icon: Icons.CarCardPen,
-    title: 'Карточка автомобиля',
+    icon: DirectionsCarIcon,
+    title: 'Список автомобилей',
   },
-  {
-    icon: Icons.CarList,
-    title: 'Список авто',
-  },
-  { icon: Icons.Orders, title: 'Заказы' },
+  { icon: AddCardIcon, title: 'Заказы' },
+  { icon: HouseIcon, title: 'Список городов' },
+  { icon: StarBorderIcon, title: 'Список тарифов' },
+  { icon: AddLocationAltIcon, title: 'Пункты выдачи' },
+  { icon: CategoryIcon, title: 'Категории автомобилей' },
+  { icon: ContactSupportIcon, title: 'Типы тарифов' },
 ];
