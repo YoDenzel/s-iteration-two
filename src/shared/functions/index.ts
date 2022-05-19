1405a0811349adf9bda89be45939278092b911d2
Denis Dolbilin <kombogame6@gmail.com>
2022-05-19 23:09:09 +0300

    refactor: deleted filter order function

--- src/shared/functions/index.ts
+++ src/shared/functions/index.ts
@@ -1,3 +1,2 @@
-export { filterDataArr } from './filter-data-arr';
 export { normalizeFilterButtonTitle } from './normalize-filter-button-title';
 export { createPaginationArray } from './create-pagination-array';
