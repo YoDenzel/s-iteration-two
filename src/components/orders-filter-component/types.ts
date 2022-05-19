234b1b5de6e52b8dc6ab603db30471be498894b0
Denis Dolbilin <kombogame6@gmail.com>
2022-05-19 23:46:35 +0300

    feat: dropdown clickhandler type

--- src/components/orders-filter-component/types.ts
+++ src/components/orders-filter-component/types.ts
@@ -4,6 +4,7 @@ export type TOrdersFilterComponent = {
   filterDataArr: Array<TFilterDataArr>;
   submitHandler: (e: React.FormEvent<HTMLDivElement>) => void;
   cancelButtonClickhandler?: () => void;
+  dropdownItemClickhandler: (activeObj: TOptionsArr, index?: number) => void;
 };
 
 type TFilterDataArr = {
