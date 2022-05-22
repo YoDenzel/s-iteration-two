64ed2c7ed1124ffbbe49b8a76303b92ed1d6d5d6
Denis Dolbilin <kombogame6@gmail.com>
2022-05-22 23:33:30 +0300

    feat: added pagination

--- src/components/cars-information/cars-information.tsx
+++ src/components/cars-information/cars-information.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from '../pagination';
 import {
   Container,
   TableCarTextInfo,
@@ -6,6 +7,7 @@ import {
   TableItem,
   TableItemIcon,
   TableItemIconBlock,
+  Wrapper,
 } from './emotion-components';
 import { TCarsInformation } from './types';
 
@@ -17,33 +19,55 @@ const rows = [
   'Заполненность бака',
 ];
 
-export function CarsInformation({ carsData }: TCarsInformation) {
+export function CarsInformation({
+  carsData,
+  currentPage,
+  isNextPage,
+  isPrevPage,
+  nextPageClickhandler,
+  paginationRange,
+  prevPageClickhandler,
+  setCurrrentPage,
+}: TCarsInformation) {
   return (
-    <Container component="section">
-      <TableCols>
-        {rows.map((item, index) => (
-          <TableCol variant="h2" key={index + item}>
-            {item}
-          </TableCol>
+    <Wrapper>
+      <Container component="section">
+        <TableCols>
+          {rows.map((item, index) => (
+            <TableCol variant="h2" key={index + item}>
+              {item}
+            </TableCol>
+          ))}
+        </TableCols>
+        {carsData?.map(item => (
+          <TableItem key={item.id}>
+            <TableItemIconBlock>
+              <TableItemIcon alt="Машина" src={item?.thumbnail?.path || ''} />
+            </TableItemIconBlock>
+            <TableCarTextInfo>
+              {item?.name || 'Нет информации'}
+            </TableCarTextInfo>
+            <TableCarTextInfo>
+              {item?.categoryId?.name || 'Нет информации'}
+            </TableCarTextInfo>
+            <TableCarTextInfo>
+              {item?.priceMin ? item.priceMin + '₽' : 'Нет информации'}
+            </TableCarTextInfo>
+            <TableCarTextInfo>
+              {item?.tank ? item.tank + '%' : 'Нет информации'}
+            </TableCarTextInfo>
+          </TableItem>
         ))}
-      </TableCols>
-      {carsData?.map(item => (
-        <TableItem key={item.id}>
-          <TableItemIconBlock>
-            <TableItemIcon src={item?.thumbnail?.path || ''} />
-          </TableItemIconBlock>
-          <TableCarTextInfo>{item?.name || 'Нет информации'}</TableCarTextInfo>
-          <TableCarTextInfo>
-            {item?.categoryId?.name || 'Нет информации'}
-          </TableCarTextInfo>
-          <TableCarTextInfo>
-            {item?.priceMin ? item.priceMin + '₽' : 'Нет информации'}
-          </TableCarTextInfo>
-          <TableCarTextInfo>
-            {item?.tank ? item.tank + '%' : 'Нет информации'}
-          </TableCarTextInfo>
-        </TableItem>
-      ))}
-    </Container>
+      </Container>
+      <Pagination
+        currentPage={currentPage}
+        isNextPage={isNextPage}
+        isPrevPage={isPrevPage}
+        nextPageClickhandler={nextPageClickhandler}
+        paginationRange={paginationRange}
+        prevPageClickhandler={prevPageClickhandler}
+        setCurrentPage={setCurrrentPage}
+      />
+    </Wrapper>
   );
 }
--- src/components/cars-information/types.ts
+++ src/components/cars-information/types.ts
@@ -2,4 +2,11 @@ import { TCarsData } from '../../shared/types';
 
 export type TCarsInformation = {
   carsData?: TCarsData[];
+  isPrevPage: () => boolean;
+  isNextPage: () => boolean;
+  currentPage: number;
+  setCurrrentPage: (v: number) => void;
+  nextPageClickhandler: () => void;
+  prevPageClickhandler: () => void;
+  paginationRange: (string | number)[] | undefined;
 };
