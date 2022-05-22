01a15157deb789c1fba702172758b3bdf6cd040d
Denis Dolbilin <kombogame6@gmail.com>
2022-05-22 23:34:01 +0300

    feat: styles

--- src/components/cars-information/emotion-components.ts
+++ src/components/cars-information/emotion-components.ts
@@ -1,11 +1,19 @@
 import styled from '@emotion/styled';
 import { Box, Typography } from '@mui/material';
 
+export const Wrapper = styled(Box)`
+  height: 100%;
+  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+`;
+
 export const Container = styled(Box)`
   display: flex;
   padding-left: ${({ theme }) => theme.spacing(2.625)}px;
   padding-right: ${({ theme }) => theme.spacing(2.625)}px;
   padding-top: ${({ theme }) => theme.spacing(2)}px;
+  padding-bottom: ${({ theme }) => theme.spacing(1)}px;
   flex-direction: column;
 `;
 
@@ -29,6 +37,7 @@ export const TableItem = styled(Box)`
   display: flex;
   align-items: center;
   margin-top: ${({ theme }) => theme.spacing(1.375)}px;
+  height: 50px;
 `;
 
 export const TableItemIconBlock = styled(Box)`
