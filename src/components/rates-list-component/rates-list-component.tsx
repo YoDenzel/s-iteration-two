8a24b8ee37cbea7081f8efe89eb046df265027ed
Denis Dolbilin <kombogame6@gmail.com>
2022-05-24 23:46:11 +0300

    feat: added loader

--- src/components/rates-list-component/rates-list-component.tsx
+++ src/components/rates-list-component/rates-list-component.tsx
@@ -1,6 +1,7 @@
 import { useCookies } from 'react-cookie';
 import { useGetData } from '../../shared/custom-hooks/use-get-data/use-get-data';
 import { TCarRate } from '../../shared/types';
+import { Loader } from '../loader';
 import { rows } from './constants';
 import {
   BodyContainer,
@@ -25,35 +26,41 @@ export function RatesListComponent() {
     token: cookie.access,
   });
 
-  console.log(data);
   return (
     <Wrapper component="main">
       <Title variant="h1">Список тарифов</Title>
       <Container component="section">
-        <TableHeader>
-          <CitiesCountBlock component="article">
-            <CitiesCountTitle variant="h2">
-              Количество тарифов:
-            </CitiesCountTitle>
-            <CitiesCountNumber variant="h2">{data?.count}</CitiesCountNumber>
-          </CitiesCountBlock>
-        </TableHeader>
-        <BodyContainer>
-          <TableCols>
-            {rows.map((item, index) => (
-              <TableCol variant="h2" key={index + item}>
-                {item}
-              </TableCol>
-            ))}
-          </TableCols>
-          {data?.data.map(item => (
-            <TableItem key={item.id}>
-              <TableRateTextInfo>{item.rateTypeId.name}</TableRateTextInfo>
-              <TableRateTextInfo>{item.price}₽</TableRateTextInfo>
-              <TableRateTextInfo>{item.rateTypeId.unit}</TableRateTextInfo>
-            </TableItem>
-          ))}
-        </BodyContainer>
+        {!isLoading && (
+          <>
+            <TableHeader>
+              <CitiesCountBlock component="article">
+                <CitiesCountTitle variant="h2">
+                  Количество тарифов:
+                </CitiesCountTitle>
+                <CitiesCountNumber variant="h2">
+                  {data?.count}
+                </CitiesCountNumber>
+              </CitiesCountBlock>
+            </TableHeader>
+            <BodyContainer>
+              <TableCols>
+                {rows.map((item, index) => (
+                  <TableCol variant="h2" key={index + item}>
+                    {item}
+                  </TableCol>
+                ))}
+              </TableCols>
+              {data?.data.map(item => (
+                <TableItem key={item.id}>
+                  <TableRateTextInfo>{item.rateTypeId.name}</TableRateTextInfo>
+                  <TableRateTextInfo>{item.price}₽</TableRateTextInfo>
+                  <TableRateTextInfo>{item.rateTypeId.unit}</TableRateTextInfo>
+                </TableItem>
+              ))}
+            </BodyContainer>
+          </>
+        )}
+        {isLoading && <Loader />}
       </Container>
     </Wrapper>
   );
