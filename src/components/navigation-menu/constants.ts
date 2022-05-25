a57e83e744f7d60f5e261fa79deccd26396a78d2
Denis Dolbilin <kombogame6@gmail.com>
2022-05-25 15:58:13 +0300

    feat: added new tab for orders status

--- src/components/navigation-menu/constants.ts
+++ src/components/navigation-menu/constants.ts
@@ -5,6 +5,7 @@ import StarBorderIcon from '@mui/icons-material/StarBorder';
 import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 import CategoryIcon from '@mui/icons-material/Category';
 import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import BeenhereIcon from '@mui/icons-material/Beenhere';
 
 export const menuTabsDataArr = [
   {
@@ -17,4 +18,5 @@ export const menuTabsDataArr = [
   { icon: AddLocationAltIcon, title: 'Пункты выдачи' },
   { icon: CategoryIcon, title: 'Категории автомобилей' },
   { icon: ContactSupportIcon, title: 'Типы тарифов' },
+  { icon: BeenhereIcon, title: 'Статусы заказов' },
 ];
