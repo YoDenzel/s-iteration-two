5c3cc64b07fa790c08d7dbe91ab9038dc83c870c
Denis Dolbilin <kombogame6@gmail.com>
2022-06-10 02:38:00 +0300

    refactor: better props

--- src/shared/api/fetch-delete-car/fetch-delete-car.ts
+++ src/shared/api/fetch-delete-car/fetch-delete-car.ts
@@ -1,7 +1,21 @@
 import { requestOptions } from '../constants';
 
-export const fetchDeleteCar = async (id: string) => {
-  const options = { ...requestOptions, method: 'DELETE', body: undefined };
+export const fetchDeleteCar = async ({
+  id,
+  token,
+}: {
+  id: string;
+  token: string;
+}) => {
+  const options = {
+    ...requestOptions,
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'X-Api-Factory-Application-Id': `${process.env.REACT_APP_API_KEY}`,
+    },
+    body: undefined,
+  };
 
   const response = await fetch(
     `https://api-factory.simbirsoft1.com/api/db/car/${id}`,
