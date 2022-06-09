7f16cca79dc9c004a584aeed8458c0d5a04b4200
Denis Dolbilin <kombogame6@gmail.com>
2022-06-09 15:03:09 +0300

    feat: moved carCardRightBlock to separate component

--- src/components/car-card/car-card.tsx
+++ src/components/car-card/car-card.tsx
@@ -7,11 +7,16 @@ import {
   setInitialColorsCheckboxArr,
   addColor,
 } from '../../redux/colors-checkbox-arr/colors-checkbox-arr';
-import { useAppDispatch, useAppSelector } from '../../shared/custom-hooks';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useDeleteCarData,
+} from '../../shared/custom-hooks';
 import { useGetData } from '../../shared/custom-hooks/use-get-data/use-get-data';
 import { Icons } from '../../shared/icons';
 import { TSingleCar } from '../../shared/types';
 import { CarCardLeftBlock } from '../car-card-left-block';
+import { CarCardRightBlock } from '../car-card-right-block';
 import { TextInputComponent } from '../text-input-component';
 
 export const Wrapper = styled(Box)`
@@ -49,168 +54,6 @@ export const CarWrapper = styled(Box)`
   height: 100%;
 `;
 
-export const CarOptionsContainer = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  flex: 75%;
-  background-color: ${({ theme }) => theme.colors.white};
-  box-shadow: 0px 2px 4px rgba(90, 97, 105, 0.12);
-  border-radius: 9px;
-  padding: ${({ theme }) => theme.spacing(2.5)}px;
-  min-height: 80%;
-  margin-bottom: ${({ theme }) => theme.spacing(2.5)}px;
-`;
-
-export const CarOptionsTitle = styled(Typography)`
-  font-style: normal;
-  font-weight: 500;
-  font-size: 17.5px;
-  line-height: 21px;
-  letter-spacing: -0.4375px;
-  color: ${({ theme }) => theme.colors.titleFont};
-`;
-
-export const EditInfrormationBlock = styled(Box)`
-  display: flex;
-  flex-wrap: wrap;
-  gap: ${({ theme }) => theme.spacing(2.625)}px;
-`;
-
-export const AvailableColorsBlock = styled(Box)`
-  flex: 100%;
-`;
-
-export const ColorsInputBlock = styled(Box)`
-  display: flex;
-  gap: ${({ theme }) => theme.spacing(1)}px;
-  align-items: flex-end;
-`;
-export const AddColor = styled(Box)`
-  padding: ${({ theme }) => theme.spacing(1.375)}px;
-  border: 1px solid ${({ theme }) => theme.colors.inputBorder};
-  border-radius: 4px;
-  height: fit-content;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  bottom: 1px;
-  cursor: pointer;
-`;
-
-export const CheckboxesBlock = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: ${({ theme }) => theme.spacing(1)}px;
-  margin-top: ${({ theme }) => theme.spacing(1.75)}px;
-  padding: 0;
-  flex: 1;
-`;
-
-export const CheckboxItem = styled.li`
-  display: flex;
-  align-items: center;
-  gap: ${({ theme }) => theme.spacing(1)}px;
-  cursor: pointer;
-  width: fit-content;
-  padding: 0;
-  text-transform: none;
-  min-width: unset;
-  padding-right: ${({ theme }) => theme.spacing(1.25)}px;
-`;
-
-export const CheckboxMark = styled(Box)<{ isactive: boolean }>`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: ${({ isactive }) => (isactive ? '#007bff' : '#ffffff')};
-  border: 1px solid #007bff;
-  border-radius: 2px;
-  width: 12px;
-  height: 12px;
-`;
-
-export const CheckboxTitle = styled(Typography)`
-  font-style: normal;
-  font-weight: 400;
-  font-size: 10px;
-  line-height: 12px;
-  letter-spacing: -0.175439px;
-  color: #495057;
-`;
-
-export const ButtonsBlock = styled(Box)`
-  display: flex;
-  justify-content: space-between;
-`;
-
-export const SaveAndCancelButtonsWrapper = styled(Box)`
-  display: flex;
-  gap: ${({ theme }) => theme.spacing(1.5)}px;
-`;
-
-export const SaveCarDataButton = styled(Button)`
-  text-transform: none;
-  background: #007bff;
-  border-radius: 4px;
-  font-family: 'Helvetica';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 11.5px;
-  line-height: 13px;
-  text-align: center;
-  letter-spacing: -0.0821429px;
-  color: #ffffff;
-  &:hover {
-    background: #1567be;
-  }
-  padding: 8px 15px 8.5px 15px;
-  border-radius: 4px;
-  height: max-content;
-`;
-
-export const CancelButton = styled(Button)`
-  text-transform: none;
-  background: #e9ecef;
-  border-radius: 4px;
-  font-family: 'Helvetica';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 11.5px;
-  line-height: 13px;
-  text-align: center;
-  letter-spacing: -0.0821429px;
-  color: #3d5170;
-  &:hover {
-    background: #cad0d8;
-  }
-  padding: 8px 15px 8.5px 15px;
-  border-radius: 4px;
-  height: max-content;
-`;
-
-export const DeleteWrapper = styled(Box)``;
-
-export const DeleteCarButton = styled(Button)`
-  text-transform: none;
-  background: #cb3656;
-  border-radius: 4px;
-  font-family: 'Helvetica';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 11.5px;
-  line-height: 13px;
-  text-align: center;
-  letter-spacing: -0.0821429px;
-  color: #ffffff;
-  &:hover {
-    background: #a0233e;
-  }
-  padding: 8px 15px 8.5px 15px;
-  border-radius: 4px;
-  height: max-content;
-`;
-
 export function CarCard() {
   const [cookie] = useCookies(['access']);
   const { carId, colorsCheckboxArr } = useAppSelector(state => ({
@@ -222,6 +65,11 @@ export function CarCard() {
     url: `car/${carId}`,
     token: cookie.access,
   });
+  const {
+    mutateAsync,
+    data: deleteResponseData,
+    isLoading: isDeleteLoading,
+  } = useDeleteCarData();
   const [image, setImage] = useState<File | null>(null);
   const [imageUrl, setImageUrl] = useState('');
   const [carInputObj, setCarInputObj] = useState({
@@ -253,15 +101,15 @@ export function CarCard() {
         }),
       );
       setCarInputObj({
-        carModelInput: data.data.name,
-        carTypeInput: data.data.categoryId.name,
-        carMinPrice: data.data.priceMin,
-        carMaxPrice: data.data.priceMax,
-        carNumber: data.data.number,
-        carDescription: data.data.description,
+        carModelInput: data?.data.name || '',
+        carTypeInput: data?.data.categoryId.name || '',
+        carMinPrice: data?.data.priceMin || '',
+        carMaxPrice: data?.data.priceMax || '',
+        carNumber: data?.data.number || '',
+        carDescription: data?.data.description || '',
       });
     }
-  }, [data]);
+  }, [data?.data]);
 
   const colorCheckboxClickhandler = (title: string) => {
     dispatch(
@@ -309,7 +157,7 @@ export function CarCard() {
     acc === 7 ? setFillingtCarPercent(100) : setFillingtCarPercent(acc * 14);
   };
 
-  const cancelChangesButtonHandler = () => {
+  const cancelChangesButtonClickhandler = () => {
     setCarInputObj({
       carDescription: data?.data?.description || 'Нет информации',
       carMaxPrice: data?.data?.priceMax || 'Нет информации',
@@ -328,9 +176,15 @@ export function CarCard() {
       );
   };
 
+  const deleteCarButtonClickhandler = () => {
+    mutateAsync(carId || '');
+  };
+
   useEffect(() => {
     progressBarFunc();
   }, [carInputObj, carColorObj]);
+
+  console.log(deleteResponseData, isDeleteLoading);
 
   return (
     <Wrapper component="main">
@@ -347,100 +201,19 @@ export function CarCard() {
           }
           progressBarPercent={fillingCarPercent}
         />
-        <CarOptionsContainer component="section">
-          <CarOptionsTitle>Настройки автомобиля</CarOptionsTitle>
-          <EditInfrormationBlock>
-            <TextInputComponent
-              type="text"
-              placeholder="Введите модель"
-              setValue={(v: string) => {
-                setCarInputObj({ ...carInputObj, carModelInput: v });
-              }}
-              value={carInputObj.carModelInput}
-              title="Модель автомобиля"
-              flex="45%"
-            />
-            <TextInputComponent
-              type="text"
-              placeholder="Введите тип автомобиля"
-              value={carInputObj.carTypeInput}
-              setValue={(v: string) =>
-                setCarInputObj({ ...carInputObj, carTypeInput: v })
-              }
-              title="Тип автомобиля"
-              flex="45%"
-            />
-            <TextInputComponent
-              type="text"
-              placeholder="Введите минимальную цену"
-              value={String(carInputObj.carMinPrice)}
-              setValue={(v: string) =>
-                setCarInputObj({ ...carInputObj, carMinPrice: v })
-              }
-              title="Мин. цена, ₽"
-              flex="25%"
-            />
-            <TextInputComponent
-              type="text"
-              placeholder="Введите максимальную цену"
-              value={String(carInputObj.carMaxPrice)}
-              setValue={(v: string) =>
-                setCarInputObj({ ...carInputObj, carMaxPrice: v })
-              }
-              title="Макс. цена, ₽"
-              flex="25%"
-            />
-            <TextInputComponent
-              type="text"
-              placeholder="Введите номер машины"
-              value={String(carInputObj.carNumber)}
-              setValue={(v: string) =>
-                setCarInputObj({ ...carInputObj, carNumber: v })
-              }
-              title="Номер машины"
-              flex="25%"
-              maxLength={9}
-            />
-            <AvailableColorsBlock>
-              <ColorsInputBlock>
-                <TextInputComponent
-                  value={carColorObj.carColorInput}
-                  setValue={(v: string) =>
-                    setCarColorObj({ ...carColorObj, carColorInput: v })
-                  }
-                  placeholder="Введите цвет"
-                  title="Доступные цвета"
-                  type="text"
-                />
-                <AddColor onClick={() => setColorClickhandler()}>
-                  <Icons.PlusIcon />
-                </AddColor>
-              </ColorsInputBlock>
-            </AvailableColorsBlock>
-          </EditInfrormationBlock>
-          <CheckboxesBlock>
-            {colorsCheckboxArr.map((item, id) => (
-              <CheckboxItem
-                key={item.title + id}
-                onClick={() => colorCheckboxClickhandler(item.title)}
-              >
-                <CheckboxMark isactive={item.isActive}>
-                  {item.isActive && <Icons.CheckMark color="#ffffff" />}
-                </CheckboxMark>
-                <CheckboxTitle variant="body1">{item.title}</CheckboxTitle>
-              </CheckboxItem>
-            ))}
-          </CheckboxesBlock>
-          <ButtonsBlock>
-            <SaveAndCancelButtonsWrapper>
-              <SaveCarDataButton>Сохранить</SaveCarDataButton>
-              <CancelButton onClick={() => cancelChangesButtonHandler()}>
-                Отменить
-              </CancelButton>
-            </SaveAndCancelButtonsWrapper>
-            <DeleteCarButton>Удалить</DeleteCarButton>
-          </ButtonsBlock>
-        </CarOptionsContainer>
+        <CarCardRightBlock
+          cancelChangesButtonClickhandler={() =>
+            cancelChangesButtonClickhandler()
+          }
+          carColorObj={carColorObj}
+          carInputObj={carInputObj}
+          colorCheckboxClickhandler={colorCheckboxClickhandler}
+          colorsCheckboxArr={colorsCheckboxArr}
+          deleteCarButtonClickhandler={() => deleteCarButtonClickhandler()}
+          setCarColorObj={setCarColorObj}
+          setCarInputObj={setCarInputObj}
+          setColorClickhandler={() => setColorClickhandler()}
+        />
       </CarWrapper>
     </Wrapper>
   );
